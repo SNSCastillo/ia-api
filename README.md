181367f746866ac88d38e8e8483e3ae4e8047a67
Santos <santosphone22@gmail.com>
2026-01-04 11:37:59 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ia-api
+
+Para fines prácticos usamos servicios de IA para iterar en ellos y asi aprovechar la capa gratuita de estas
---
+++ index.ts
@@ -0,0 +1,8 @@
+const server = Bun.serve({
+  port: 3000,
+  fetch(request) {
+    return new Response("Hello, Bun!");
+  },
+});
+
+console.log(`Servidor corriendo en http://localhost:${server.port}`);
